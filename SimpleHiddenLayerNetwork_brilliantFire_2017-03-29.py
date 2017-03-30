32dbd39f713a7a90fef6a0e1daabd7c5ec569e81
brilliantFire <rlvislaywade@outlook.com>
2017-03-30 16:10:55 -0400

    Update SimpleHiddenLayerNetwork_brilliantFire_2017-03-29.py

--- SimpleHiddenLayerNetwork_brilliantFire_2017-03-29.py
+++ SimpleHiddenLayerNetwork_brilliantFire_2017-03-29.py
@@ -30,8 +30,8 @@ np.random.seed(123)    # So we all have the same random numbers
 synapse0 = 1 - 2*np.random.random((3,4))        # inputLayer -> hiddenLayer
 synapse1 = 1 - 2*np.random.random((4,1))        # hiddenLayer -> outputLayer
 
-for trainingRun in xrange(100000):
-    if (trainingRun% 10000) == 0:
+for trainingRun in xrange(10000):
+    if (trainingRun% 1000) == 0:
         print "Training Run # %s " %trainingRun
         
     # FEEDFORWARD: inputLayer -> hiddenLayer -> outputLayer 
@@ -39,13 +39,13 @@ for trainingRun in xrange(100000):
     hiddenLayer = nonlinWeightingFunc(np.dot(inputLayer,synapse0))
     outputLayer = nonlinWeightingFunc(np.dot(hiddenLayer,synapse1))
     
-    if (trainingRun% 10000) == 0:
+    if (trainingRun% 1000) == 0:
         print "Output Guess: \n" + str(outputLayer)
 
     # How close did the outputLayer get to the desiredOutput?
     outputLayer_error = desiredOutput - outputLayer
     
-    if (trainingRun% 10000) == 0:
+    if (trainingRun% 1000) == 0:
         print "Output Layer Error: " + str(np.mean(np.abs(outputLayer_error)))
         
     # By how much should we change the weights on synapse1 (hiddenLayer -> outputLayer)?
@@ -54,7 +54,7 @@ for trainingRun in xrange(100000):
     # How much did each hiddenLayer value contribute to outputLayer error (according to the weights)?
     hiddenLayer_error = outputLayer_delta.dot(synapse1.T)
     
-    if (trainingRun% 10000) == 0:
+    if (trainingRun% 1000) == 0:
         print "Hidden Layer Error: " + str(np.mean(np.abs(hiddenLayer_error))) + "\n"
     
     # By how much should we change the weights on synapse0 (inputLayer -> outputLayer)?
