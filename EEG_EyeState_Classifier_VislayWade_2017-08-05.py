93637b85020ab94ab4dc0a34e09ac011f53a73b1
Rebecca Vislay Wade <rlvislaywade@outlook.com>
2017-08-05 14:08:15 -0400

    Deep EEG Eye State Classifier
    This program builds a deep neural network model of eye state (eyes open or closed) from EEG signals recorded on 14 different scalp electrodes. More info on this dataset can be found here: https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State#

---
+++ EEG_EyeState_Classifier_VislayWade_2017-08-05.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 18:31:55 2017
+
+"""
+# A second network using the keras library - EEG Eye State data
+# Rebecca Vislay Wade
+# PREDICT 413 | Summer 2017
+
+# (IN DEVELOPMENT) This program builds a deep neural network model of eye state
+# (eyes open or closed) from EEG signals recorded on 14 different scalp 
+# electrodes. More info on this dataset can be found here:
+# https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State#
+
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+from pandas import DataFrame, crosstab
+from sklearn.model_selection import train_test_split
+
+# Random number seed
+numpy.random.seed(420)
+
+# EEG Eye State dataset - (freely available at 
+# https://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff )
+# Copy-paste data into text editor and save as "EEGeyebehavior.csv" in working
+# directory.
+# Response (column 14) coded 1 for eyes closed and 0 for eyes open
+eeg=numpy.loadtxt("EEGeyebehavior.csv", delimiter = ",")
+
+# convert numpy array to pandas Series to do train/test split and look at 
+# actual values of TARGET
+eegDF=DataFrame(eeg)
+
+# 70/30 training/test split
+X_train, X_test, y_train, y_test = train_test_split(eegDF, eegDF.iloc[:,14], 
+                                                    test_size=0.3)
+# Drop the response variable from the X's
+X_train = X_train.drop(14,1)
+X_test = X_test.drop(14,1)
+
+# Actual values for TARGET from EEG dataset (frequency table)
+crosstab(index=eegDF.iloc[:,14], columns="count") 
+
+# Checking the split (also frequency tables)
+crosstab(index=y_train, columns="count")
+crosstab(index=y_test, columns="count")
+
+# Convert splits back to numpy arrays for Keras functions
+X_trainArr = numpy.array(X_train)
+y_trainArr = numpy.array(y_train)
+X_testArr = numpy.array(X_test)
+y_testArr = numpy.array(y_test)
+
+# Create a sequential model
+model = Sequential()
+# Add model layers
+    # "Dense" indicates a fully connected network
+    #
+    # input_dim - the number of predictors.
+    #
+    # Sigmoid activations for all neurons (could change in subsequent versions)
+
+model.add(Dense(14, input_dim = 14, activation = 'sigmoid'))
+model.add(Dense(20, activation = 'sigmoid'))
+model.add(Dense(10, activation = 'sigmoid'))
+model.add(Dense(10, activation = 'sigmoid'))
+model.add(Dense(1, activation = 'sigmoid'))
+
+# Compile model
+    # This is where the tensor calculations are being done by the library of
+    # fast numerical functions in TensorFlow (or Theano).
+    #
+    # Some things about the compile function:
+        # loss argument - specifies the loss function used to check the 
+        # weights; Here, it is the log-loss function ('binary_crossentropy'). 
+        # This is the function the model is minimizing.
+        #
+        # optimizer argument - specifies the search algorithm for finding the 
+        # best set of weights; 'adam' specifies an efficient default for
+        # the method of finding the best set of weights
+        #
+        # metrics argument - the metric(s) reported after training; here, we're
+        # reporting classification accuracy.
+model.compile(loss='binary_crossentropy', 
+              optimizer='sgd', metrics=['accuracy'])
+
+# Fit the model
+    # Lots and lots of matrix operations are performed. This step can be 
+    # distributed to graphics cards.
+    # epochs - number of training iterations
+    #
+    # batch_size - number of samples per gradiant update
+    #
+    # validation_split - percentage of training samples to use for weight set
+    # validation
+model.fit(X_trainArr, y_trainArr, epochs=30000, batch_size=100, 
+          validation_split = 0.3)
+
+# Training accuracy - how well does the model fit the training data?
+scores = model.evaluate(X_trainArr, y_trainArr)
+print("\nTraining Accuracy: %.2f%%" % (scores[1]*100))
+
+# Test accuracy - how well does the model predict data it has not been trained
+# on?
+# First, calculate predictions and round them (50% decision rule)
+preds = model.predict(y_testArr)
+roundPreds = [round(x[0]) for x in preds]
+
+#### UNFINISHED ####
