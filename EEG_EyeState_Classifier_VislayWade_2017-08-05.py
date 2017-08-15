66f38b18e25e4f8c053d57e3abf42d929ff7aa25
Rebecca Vislay Wade <rlvislaywade@outlook.com>
2017-08-15 16:24:35 -0400

    Update EEG_EyeState_Classifier_VislayWade_2017-08-05.py
    8/15/2017 - Added predictor scaling, out-of-sample confusion matrix and classification accuracy calculations.

--- EEG_EyeState_Classifier_VislayWade_2017-08-05.py
+++ EEG_EyeState_Classifier_VislayWade_2017-08-05.py
@@ -7,19 +7,20 @@ Created on Thu Aug  3 18:31:55 2017
 # Rebecca Vislay Wade
 # PREDICT 413 | Summer 2017
 
-# (IN DEVELOPMENT) This program builds a deep neural network model of eye state
+# This program builds a deep neural network model of eye state
 # (eyes open or closed) from EEG signals recorded on 14 different scalp 
 # electrodes. More info on this dataset can be found here:
 # https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State#
 
+# UPDATED 15 Aug 2017: Added data scaling and computation of test set
+# confusion matrix and classification accuracy.
+
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
-from pandas import DataFrame, crosstab
+from pandas import DataFrame, Series, crosstab
 from sklearn.model_selection import train_test_split
-
-# Random number seed
-numpy.random.seed(420)
+from sklearn import preprocessing
 
 # EEG Eye State dataset - (freely available at 
 # https://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff )
@@ -32,12 +33,19 @@ eeg=numpy.loadtxt("EEGeyebehavior.csv", delimiter = ",")
 # actual values of TARGET
 eegDF=DataFrame(eeg)
 
+# Random number seed
+numpy.random.seed(420)
+
 # 70/30 training/test split
 X_train, X_test, y_train, y_test = train_test_split(eegDF, eegDF.iloc[:,14], 
                                                     test_size=0.3)
 # Drop the response variable from the X's
 X_train = X_train.drop(14,1)
 X_test = X_test.drop(14,1)
+
+# Normalize predictors
+X_trainNorm = preprocessing.scale(X_train)
+X_testNorm = preprocessing.scale(X_test)
 
 # Actual values for TARGET from EEG dataset (frequency table)
 crosstab(index=eegDF.iloc[:,14], columns="count") 
@@ -47,10 +55,13 @@ crosstab(index=y_train, columns="count")
 crosstab(index=y_test, columns="count")
 
 # Convert splits back to numpy arrays for Keras functions
-X_trainArr = numpy.array(X_train)
+X_trainArr = numpy.array(X_trainNorm)
 y_trainArr = numpy.array(y_train)
-X_testArr = numpy.array(X_test)
+X_testArr = numpy.array(X_testNorm)
 y_testArr = numpy.array(y_test)
+
+# Random number seed
+numpy.random.seed(420)
 
 # Create a sequential model
 model = Sequential()
@@ -83,7 +94,7 @@ model.add(Dense(1, activation = 'sigmoid'))
         # metrics argument - the metric(s) reported after training; here, we're
         # reporting classification accuracy.
 model.compile(loss='binary_crossentropy', 
-              optimizer='sgd', metrics=['accuracy'])
+              optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
     # Lots and lots of matrix operations are performed. This step can be 
@@ -94,8 +105,7 @@ model.compile(loss='binary_crossentropy',
     #
     # validation_split - percentage of training samples to use for weight set
     # validation
-model.fit(X_trainArr, y_trainArr, epochs=30000, batch_size=100, 
-          validation_split = 0.3)
+model.fit(X_trainArr, y_trainArr, epochs=10000, batch_size=100)
 
 # Training accuracy - how well does the model fit the training data?
 scores = model.evaluate(X_trainArr, y_trainArr)
@@ -103,8 +113,13 @@ print("\nTraining Accuracy: %.2f%%" % (scores[1]*100))
 
 # Test accuracy - how well does the model predict data it has not been trained
 # on?
-# First, calculate predictions and round them (50% decision rule)
-preds = model.predict(y_testArr)
-roundPreds = [round(x[0]) for x in preds]
+# First, calculate predictions and round them (0.5 decision rule)
+preds = model.predict(X_testArr, batch_size = 10, verbose = 1)
+roundPreds = Series([round(x[0]) for x in preds])
 
-#### UNFINISHED ####
+# Confusion matrix for predictions
+confMat = crosstab(index=roundPreds, columns=y_test, margins = True)
+
+# Test set classification accuracy
+print("\nTest Accuracy: %.2f%%" % ((confMat.loc[0,0] + confMat.loc[1,1])/
+                                       (confMat.loc["All","All"])*100))
